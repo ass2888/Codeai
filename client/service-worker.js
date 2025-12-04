266dcadc00c7894a90c7a0381cb474573c028ecb
ass2888 <aseelm9988@gmail.com>
2025-12-04 11:45:18 +0300

    Add files via upload

---
+++ client/service-worker.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'codeai-static-v1.266';
+const ASSETS = [
+'/',
+'/index.html',
+'/manifest.json',
+'/styles.css',
+'/app.js',
+'/icons/icon-192x192.png', 
+'/icons/icon-512x512.png'];
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', (e) => {
+  if(e.request.method !== 'GET') return;
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
